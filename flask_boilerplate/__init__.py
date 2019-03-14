375fa8d786e5385e3580803814fe39608fb29b88
LazaroFranco <lazaro300x@icloud.com>
2019-03-14 16:15:21 -0400

    Add boilerplate test
    	- Modify create_app factory to take config options for different environments
    	- create pytest configuration file with fixtures to create an app and client object for each test
    Create simple test for factory and index route:

--- flask_boilerplate/__init__.py
+++ flask_boilerplate/__init__.py
@@ -1,10 +1,21 @@
 from flask import Flask
-	
-def create_app():
-	app = Flask(__name__)
-	
-	@app.route('/')
-	def index():
-		return 'Hello, World'
-	
-        return app
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    @app.route('/')
+    def index():
+        return 'Hello World!'
+
+    return app
+
